6eac94ae2af48cf8d692bfd8658cdc8ec368d46c
unknown <noey.rachata@gmail.com>
2024-10-23 18:03:02 +0700

    Update project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,35 @@
-import React from "react";
-
-//images
+import React, { useState } from "react";
+// images
 import gbacLogo from "../assets/images/GBAC-logo.jpg";
 
 const Navbar = () => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
+
   return (
     <div>
       <nav className="bg-white backdrop-filter backdrop-blur-lg bg-opacity-30 border-b border-gray-400 fixed w-full z-20 top-0 start-0">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
-          <a
-            href="#"
-            className="flex items-center space-x-3 rtl:space-x-reverse"
-          >
+        <div className="max-w-screen-laptop flex items-center justify-between mx-auto p-2">
+          <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
             <img src={gbacLogo} className="w-24 h-15" alt="GBAC Logo" />
           </a>
-          <div className=" flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+
+          {/* Hamburger menu for mobile screens */}
+          <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
             <button
-              type="button"
-              className="text-white bg-blue-950 hover:bg-blue-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-normal rounded-md text-sm px-4 py-2 text-center"
-            >
-              Join GBAC
-            </button>
-            <button
-              data-collapse-toggle="navbar-sticky"
+              onClick={toggleMobileMenu}
               type="button"
               className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
               aria-controls="navbar-sticky"
-              aria-expanded="false"
+              aria-expanded={mobileMenuOpen ? "true" : "false"}
             >
               <span className="sr-only">Open main menu</span>
               <svg
@@ -46,15 +49,14 @@ const Navbar = () => {
               </svg>
             </button>
           </div>
-          <div
-            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-            id="navbar-sticky"
-          >
-            <ul className="flex flex-col p-4 md:p-0 mt-4 font-light drop-shadow-xl border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
+
+          {/* Full menu for medium and larger screens */}
+          <div className="hidden md:flex ml-20 items-end justify-between w-full">
+            <ul className="flex flex-col md:flex-row p-4 md:p-0 mt-4 font-light drop-shadow-xl border border-gray-100 rounded-lg rtl:space-x-reverse md:mt-0 md:border-0 justify-between w-full">
               <li>
                 <a
                   href="#"
-                  className="block py-2 px-3 text-black border-b-2 border-black bg-blue-700 md:bg-transparent md:text-black-700 md:p-0"
+                  className="block py-2 px-3 text-black border-b-2 border-black md:bg-transparent md:text-black-700 md:p-0"
                   aria-current="page"
                 >
                   Home
@@ -79,7 +81,7 @@ const Navbar = () => {
               <li>
                 <a
                   href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 hidden md:p-0 lg:flex lg:hover:bg-transparent lg:hover:text-blue-700"
                 >
                   Locations
                 </a>
@@ -103,7 +105,7 @@ const Navbar = () => {
               <li>
                 <a
                   href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 hidden md:p-0 lg:flex lg:hover:bg-transparent lg:hover:text-blue-700"
                 >
                   Giants
                 </a>
@@ -111,21 +113,21 @@ const Navbar = () => {
               <li>
                 <a
                   href="#"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 hidden md:p-0 lg:flex lg:hover:bg-transparent lg:hover:text-blue-700"
                 >
                   Our Sponsors
                 </a>
               </li>
-              <li>
-                {/* Dropdown menu */}
+              <li className="relative">
+                {/* Dropdown menu within relative container */}
                 <button
+                  onClick={toggleDropdown}
                   id="dropdownNavbarLink"
-                  data-dropdown-toggle="dropdownNavbar"
-                  class="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto"
+                  className="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto"
                 >
                   More{" "}
                   <svg
-                    class="w-2.5 h-2.5 ms-2.5"
+                    className="w-2.5 h-2.5 ms-2.5"
                     aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -133,58 +135,177 @@ const Navbar = () => {
                   >
                     <path
                       stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
                       d="m1 1 4 4 4-4"
                     />
                   </svg>
                 </button>
                 <div
                   id="dropdownNavbar"
-                  class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44"
+                  className={`z-10 ${
+                    dropdownOpen ? "block" : "hidden"
+                  } font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 transition-all duration-300 ease-in-out absolute ${
+                    dropdownOpen ? "opacity-100 scale-100" : "opacity-0 scale-95"
+                  } left-0 top-full mt-2`}
                 >
-                  <ul
-                    class="py-2 text-sm"
-                    aria-labelledby="dropdownLargeButton"
-                  >
+                  <ul className="py-2 text-sm" aria-labelledby="dropdownLargeButton">
                     <li>
-                      <a href="#" class="block px-4 py-2 hover:bg-gray-100">
+                      <a href="#" className="block px-4 py-2 hover:bg-gray-100">
                         Join GBAC
                       </a>
                     </li>
                     <li>
-                      <a href="#" class="block px-4 py-2 hover:bg-gray-100">
+                      <a href="#" className="block px-4 py-2 hover:bg-gray-100">
                         Handbook
                       </a>
                     </li>
                     <li>
-                      <a href="#" class="block px-4 py-2 hover:bg-gray-100">
+                      <a href="#" className="block px-4 py-2 hover:bg-gray-100">
                         Basketball Rules
                       </a>
                     </li>
                     <li>
-                      <a href="#" class="block px-4 py-2 hover:bg-gray-100">
+                      <a href="#" className="block px-4 py-2 hover:bg-gray-100">
                         Volleyball Rules
                       </a>
                     </li>
                     <li>
-                      <a href="#" class="block px-4 py-2 hover:bg-gray-100">
+                      <a href="#" className="block px-4 py-2 hover:bg-gray-100">
                         Medical Partners
                       </a>
                     </li>
                     <li>
-                      <a href="#" class="block px-4 py-2 hover:bg-gray-100">
+                      <a href="#" className="block px-4 py-2 hover:bg-gray-100">
                         Core and Expansion Sports
                       </a>
                     </li>
                     <li>
-                      <a href="#" class="block px-4 py-2 hover:bg-gray-100">
+                      <a href="#" className="block px-4 py-2 hover:bg-gray-100">
                         Age Groups
                       </a>
                     </li>
                   </ul>
                 </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        {/* Mobile menu positioned outside and under the navbar with w-64 */}
+        <div
+          className={`${
+            mobileMenuOpen ? "block" : "hidden"
+          } absolute w-64 bg-white shadow-lg z-10 left-0 top-full mt-2`}
+        >
+          <ul className="flex flex-col p-4 font-light divide-y divide-gray-100">
+            <li>
+              <a href="#" className="block py-2 px-3 text-gray-900">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className="block py-2 px-3 text-gray-900">
+                Member Schools
+              </a>
+            </li>
+            <li>
+              <a href="#" className="block py-2 px-3 text-gray-900">
+                Contacts
+              </a>
+            </li>
+            <li>
+              <a href="#" className="block py-2 px-3 text-gray-900">
+                Locations
+              </a>
+            </li>
+            <li>
+              <a href="#" className="block py-2 px-3 text-gray-900">
+                Calendar
+              </a>
+            </li>
+            <li>
+              <a href="#" className="block py-2 px-3 text-gray-900">
+                All-Stars
+              </a>
+            </li>
+            <li>
+              <a href="#" className="block py-2 px-3 text-gray-900">
+                Giants
+              </a>
+            </li>
+            <li>
+              <a href="#" className="block py-2 px-3 text-gray-900">
+                Our Sponsors
+              </a>
+            </li>
+            <li>
+              <button
+                onClick={toggleDropdown}
+                className="flex justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100"
+              >
+                More{" "}
+                <svg
+                  className="w-2.5 h-2.5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 10 6"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 1 4 4 4-4"
+                  />
+                </svg>
+              </button>
+            </li>
+          </ul>
+
+          {/* Dropdown for "More" with w-44 */}
+          <div
+            className={`${
+              dropdownOpen ? "block" : "hidden"
+            } absolute w-44 bg-white shadow-lg z-10 left-0 mt-2`}
+          >
+            <ul className="flex flex-col p-4 font-light divide-y divide-gray-100">
+              <li>
+                <a href="#" className="block px-4 py-2 hover:bg-gray-100">
+                  Join GBAC
+                </a>
+              </li>
+              <li>
+                <a href="#" className="block px-4 py-2 hover:bg-gray-100">
+                  Handbook
+                </a>
+              </li>
+              <li>
+                <a href="#" className="block px-4 py-2 hover:bg-gray-100">
+                  Basketball Rules
+                </a>
+              </li>
+              <li>
+                <a href="#" className="block px-4 py-2 hover:bg-gray-100">
+                  Volleyball Rules
+                </a>
+              </li>
+              <li>
+                <a href="#" className="block px-4 py-2 hover:bg-gray-100">
+                  Medical Partners
+                </a>
+              </li>
+              <li>
+                <a href="#" className="block px-4 py-2 hover:bg-gray-100">
+                  Core and Expansion Sports
+                </a>
+              </li>
+              <li>
+                <a href="#" className="block px-4 py-2 hover:bg-gray-100">
+                  Age Groups
+                </a>
               </li>
             </ul>
           </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,30 @@
 /** @type {import('tailwindcss').Config} */
-
 const withMT = require("@material-tailwind/react/utils/withMT");
+const flowbite = require("flowbite-react/tailwind");
 
 module.exports = withMT({
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html", 
+    "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
+  ],
   theme: {
     extend: {
       fontFamily: {
         proxima: ["Proximanova", "sans-serif"],
       },
+        colors: {
+          transparent: 'transparent',
+          current: 'currentColor',
+          'white': '#ffffff',
+
+        },screens: {
+          'laptop': '1000px',
+
+        },
     },
   },
-  plugins: [],
+  plugins: [
+    flowbite.plugin(),
+  ],
 });
